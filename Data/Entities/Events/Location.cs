bad328b6c96d0cf8e2a544cb1c8cf9898b19f6cd
AnnaBarczyk <56827312+AnnaBarczyk@users.noreply.github.com>
2020-08-21 09:53:50 +0200

    Fix 1.0 Entities properties

--- Data/Entities/Events/Location.cs
+++ Data/Entities/Events/Location.cs
@@ -7,8 +7,8 @@ namespace BeforeApp.Data.Entities
 {
     public class Location
     {
-        public int LocationId { get; set; }
-        public string PlaceName { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
         public string City { get; set; }
         public string Adress { get; set; }
         public ICollection<Event> Events { get; set; }
