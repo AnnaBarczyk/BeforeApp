5fb18ae10cde6d851fb9e37187faba6071c7bd47
MaciejBiernat <57531118+MaciejBiernat@users.noreply.github.com>
2020-08-26 10:48:43 +0200

    Implement swagger documentation in startup.cs

--- Startup.cs
+++ Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace BeforeApp
 {
@@ -37,6 +38,11 @@ namespace BeforeApp
             services.AddControllers();
             
             services.AddMvc(opt => opt.EnableEndpointRouting = false);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BeforeApp", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +54,13 @@ namespace BeforeApp
             }
 
             app.UseMvc();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
         }
     }
 }
